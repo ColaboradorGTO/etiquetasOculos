d9cccce81923b36de0dc2ffe40289ee8a5adfda4
Myltiane Alves <myltiane.aux@gmail.com>
2026-09-25 15:29:22 -0300

    refactor:  zplResetConfiguracao

--- src/componets/Etiquetagem/Components/ActionProdutoEtiqueta/ActionEtiquetaOculos/actionEtiquetaOculosModal.jsx
+++ src/componets/Etiquetagem/Components/ActionProdutoEtiqueta/ActionEtiquetaOculos/actionEtiquetaOculosModal.jsx
@@ -40,6 +40,7 @@ export const ActionEtiquetaOculosModal = ({
     try {
       let startPageLabel = `
         ^XA
+        ^PON
         ^FWN
         ^PW800
         ^LL80
@@ -80,9 +81,9 @@ export const ActionEtiquetaOculosModal = ({
 
           dataLabelsZPL += `
             ${startPageLabel}
-            ^FO5,25^A0,40,${widthFontPrecoVenda}^FB268,1,1,C,0^FD${precoVenda}^FS
+            ^FO5,45^A0,40,${widthFontPrecoVenda}^FB268,1,1,C,0^FD${precoVenda}^FS
             ^BY1.6,3,500
-            ^FO280,10
+            ^FO290,20
             ^BEN,55,Y,N
             ^FD${codBarras}^FS
             ${endPageLabel}
