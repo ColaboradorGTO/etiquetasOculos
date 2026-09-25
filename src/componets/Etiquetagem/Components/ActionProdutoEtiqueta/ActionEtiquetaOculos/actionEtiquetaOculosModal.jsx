e323daec0bc9733fa46c34ae013a2ecc832ae9c5
Myltiane Alves <myltiane.aux@gmail.com>
2026-09-25 14:24:30 -0300

    refactor:  zplResetConfiguracao

--- src/componets/Etiquetagem/Components/ActionProdutoEtiqueta/ActionEtiquetaOculos/actionEtiquetaOculosModal.jsx
+++ src/componets/Etiquetagem/Components/ActionProdutoEtiqueta/ActionEtiquetaOculos/actionEtiquetaOculosModal.jsx
@@ -40,7 +40,6 @@ export const ActionEtiquetaOculosModal = ({
     try {
       let startPageLabel = `
         ^XA
-        ^MD10
         ^FWN
         ^PW800
         ^LL80
@@ -48,7 +47,7 @@ export const ActionEtiquetaOculosModal = ({
       `;
       const zplResetConfiguracao = `
         ^XA
-        ^MD10
+        ^MD5
         ^FWN
         ^PW800
         ^LL80
